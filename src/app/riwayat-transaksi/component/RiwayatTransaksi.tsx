d692c267ecf500174e5beb3d012ecf04c7112861
Ahmad Rizki <amrizjr@gmail.com>
2023-11-05 21:46:33 +0700

    feat: add riwayat-transaksi router

---
+++ src/app/riwayat-transaksi/component/RiwayatTransaksi.tsx
@@ -0,0 +1,7 @@
+export default function RiwayatTransaksi() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Riwayat Transaksi</h1>
+        </main>
+    );
+}
