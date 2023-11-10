f2653acc184456143f220546be99a828ba3ff0a3
frendysanusi05 <frendyvivo5@gmail.com>
2023-11-10 15:14:44 +0700

    feat: add filter pakaian, change components & seeders

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -2,8 +2,8 @@ import KatalogPakaian from "./components/KatalogPakaian";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <>
             <KatalogPakaian />
-        </main>
+        </>
     );
 }
