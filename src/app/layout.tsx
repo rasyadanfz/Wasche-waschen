3765d298cd4f36ce2f5d8c31e0a1dc9896735626
Ahmad Rizki <amrizjr@gmail.com>
2023-11-12 05:02:20 +0700

    feat: not showing navbar when in login-register page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,15 +14,19 @@ export const metadata: Metadata = {
   }
 };
 
+const disabledNavbar = ['/login', '/register'];
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  const pathName = usePathname();
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        {!disabledNavbar.includes(pathName) && <Navbar />}
         {children}
       </body>
     </html>
