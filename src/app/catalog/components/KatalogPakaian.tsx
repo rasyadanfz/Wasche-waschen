7fa6c1f1003edaea5c326674586a2231de4d188f
frendysanusi05 <frendyvivo5@gmail.com>
2023-11-14 07:46:37 +0700

    fix: search bar pakaian

--- src/app/catalog/components/KatalogPakaian.tsx
+++ src/app/catalog/components/KatalogPakaian.tsx
@@ -2,7 +2,8 @@
 
 import { FaSearch } from "react-icons/fa";
 import { useEffect, useState } from "react";
-import Pakaian from "./Pakaian"
+import PakaianComponent from "./PakaianComponent";
+import { Pakaian } from "@prisma/client";
 
 async function getDataPakaian() {
   const res = await fetch("/api/pakaian", {
@@ -20,6 +21,7 @@ const KatalogPakaian = () => {
     const fetchData = async() => {
       const data = await getDataPakaian();
       setDataPakaian(data);
+      setFilteredData(data);
     };
 
     fetchData();
@@ -29,8 +31,7 @@ const KatalogPakaian = () => {
   const [filteredData, setFilteredData] = useState(dataPakaian);
 
   const handleSearch = () => {
-    // still have bugs, don't know where
-    const result = dataPakaian.filter((item) => {
+    const result = dataPakaian.filter((item: Pakaian) => {
       const nameMatch = item.name.toLowerCase().includes(query.toLowerCase());
       return nameMatch;
     });
@@ -57,15 +58,15 @@ const KatalogPakaian = () => {
             <div className="flex justify-end mt-2">
               <button
                 className="px-4 py-2 bg-transparent rounded-md border text-black border-black font-semibold h-14 active:bg-primary active:text-white"
-                onClick={handleSearch} // still have bugs, don't know where
+                onClick={handleSearch}
               >
                 <FaSearch size={18} />
               </button>
             </div>
           </div>
           <div>
-            {dataPakaian.map((pakaian) => (
-              <Pakaian pakaian={pakaian} key={pakaian.id} />
+            {filteredData.map((pakaian: Pakaian) => (
+              <PakaianComponent pakaian={pakaian} key={pakaian.id} />
               ))}
           </div>
         </div>
