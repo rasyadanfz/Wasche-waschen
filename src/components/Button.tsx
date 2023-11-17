786d86ea96c402257eb4b6d3744afd112ba95f10
Rasyadan Faza <18221103@std.stei.itb.ac.id>
2023-11-18 00:07:59 +0700

    feat: add more button color variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -17,20 +17,45 @@ const Button = ({
     const baseClassName =
         "text-button px-4 py-2 rounded-md w-full h-full font-raleway";
 
-    const buttonClassName = baseClassName + " " + className;
+    let buttonColorClass;
+    switch (type) {
+        case "primary":
+            buttonColorClass =
+                "bg-primary-400 hover:bg-primary-300 active:bg-primary-300";
+            break;
+        case "secondary":
+            buttonColorClass =
+                "bg-secondary-400 hover:bg-secondary-300 active:bg-secondary-300";
+            break;
+        case "success":
+            buttonColorClass =
+                "bg-success-400 hover:bg-success-300 active:bg-success-300";
+            break;
+        case "warning":
+            buttonColorClass =
+                "bg-warning-400 hover:bg-warning-300 active:bg-warning-300 ";
+            break;
+        case "danger":
+            buttonColorClass =
+                "bg-danger-400 hover:bg-danger-300 active:bg-danger-300";
+            break;
+    }
+
+    const buttonClassName =
+        baseClassName + " " + buttonColorClass + " " + className;
     return (
         <>
             <button
                 type="submit"
-                className={`${
-                    type === "primary"
-                        ? "bg-primary-400 hover:bg-primary-300 active:bg-primary-300"
-                        : "bg-secondary-400 hover:bg-secondary-300 active:bg-secondary-300"
-                } ${buttonClassName}`}
+                className={buttonClassName}
                 onClick={onClick}
                 id={id}
             >
-                <div className="text-white">{text}</div>
+                <div
+                    className={type === "warning" ? "text-black" : "text-white"}
+                >
+                    {text}
+                </div>
             </button>
         </>
     );
