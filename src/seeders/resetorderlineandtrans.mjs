9698970bf80a1954da126fcfc6c6772d636c244d
Rasyadan Faza <18221103@std.stei.itb.ac.id>
2023-11-19 21:19:32 +0700

    feat: add data seeders

---
+++ src/seeders/resetorderlineandtrans.mjs
@@ -0,0 +1,15 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.orderline.deleteMany();
+    await prisma.transaksi.deleteMany();
+    await prisma.user.delete({
+        where: {
+            email: "test12345@gmail.com",
+        },
+    });
+}
+
+main();
