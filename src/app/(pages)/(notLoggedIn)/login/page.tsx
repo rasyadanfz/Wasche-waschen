686a40ea8945c180e71dbc2fc3d07f61988e2906
Rasyadan Faza <18221103@std.stei.itb.ac.id>
2023-11-21 23:55:00 +0700

    fix: bug fixes

--- src/app/(pages)/(notLoggedIn)/login/page.tsx
+++ src/app/(pages)/(notLoggedIn)/login/page.tsx
@@ -26,7 +26,7 @@ const LoginPage = async () => {
                 </Link>
             </div>
             <div className="lg:grid lg:grid-cols-2 lg:gap-x-12 lg:items-center lg:pb-6 lg:mt-14">
-                <div className="flex flex-col px-10 py-4 mt-3 mr-2 font-raleway lg:border-2 lg:border-black  rounded-lg">
+                <div className="flex flex-col px-10 py-4 mt-3 mr-2 font-raleway lg:border-2 lg:border-black rounded-lg lg:bg-[#FBFBFB]">
                     <div className="flex lg:hidden justify-center mb-5">
                         <Image
                             alt="Wasche Waschen Logo"
