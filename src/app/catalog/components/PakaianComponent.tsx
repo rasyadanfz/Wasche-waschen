ce0230a42be73b25d9802999cb2cb99cc3d7750d
frendysanusi <partner.frendysanusi@gmail.com>
2023-11-21 11:23:37 +0700

    style: update ui

--- src/app/catalog/components/PakaianComponent.tsx
+++ src/app/catalog/components/PakaianComponent.tsx
@@ -4,17 +4,17 @@ import AddButton from "./AddButton";
 
 const PakaianComponent = ({ pakaian }: { pakaian: Pakaian }) => {
     return (
-        <div className="border border-slate-300 flex flex-col justify-between my-5 items-start gap-4">
+        <div className="border flex flex-col justify-between my-5 items-start gap-4 shadow-md">
             <div className="">
                 <Image
                     src={`/assets/${pakaian.name}.jpg`}
                     alt={pakaian.name}
                     width={144}
                     height={144}
-                    className="w-full h-[144px]"
+                    className="w-[300px] h-[144px]"
                 />
             </div>
-            <div className="p-4">
+            <div className="p-4 w-full">
                 <h2 className="font-bold text-2xl">{pakaian.name}</h2>
                 <p>Rp{pakaian.price}/{pakaian.unit}</p>
             </div>
