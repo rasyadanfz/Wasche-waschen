07a9e2fd4f260a6eea51eb0ee8d398acbb07395c
Ahmad Rizki <amrizjr@gmail.com>
2023-11-23 07:55:14 +0700

    style: change filter transaksi and riwayat transaksi

--- src/app/(pages)/(loggedIn)/catalog/components/KatalogPakaian.tsx
+++ src/app/(pages)/(loggedIn)/catalog/components/KatalogPakaian.tsx
@@ -10,121 +10,116 @@ import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 
 async function getDataPakaian() {
-    const res = await fetch("/api/pakaian", {
-        method: "GET",
-    });
+  const res = await fetch("/api/pakaian", {
+    method: "GET",
+  });
 
-    const dataPakaian = await res.json();
-    return dataPakaian;
+  const dataPakaian = await res.json();
+  return dataPakaian;
 }
 
 const KatalogPakaian = () => {
-    const session = useSession();
-    if (!session) {
-        redirect("/login");
-    }
+  const session = useSession();
+  if (!session) {
+    redirect("/login");
+  }
 
-    const [dataPakaian, setDataPakaian] = useState([]);
-    const [query, setQuery] = useState("");
-    const [filteredData, setFilteredData] = useState(dataPakaian);
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 8;
+  const [dataPakaian, setDataPakaian] = useState([]);
+  const [query, setQuery] = useState("");
+  const [filteredData, setFilteredData] = useState(dataPakaian);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
 
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
 
-    const onHandlePage = (pageNumber: number) => {
-        setCurrentPage(pageNumber);
+  const onHandlePage = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getDataPakaian();
+      setDataPakaian(data);
+      setFilteredData(data);
     };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = await getDataPakaian();
-            setDataPakaian(data);
-            setFilteredData(data);
-        };
+    fetchData();
+  }, []);
 
-        fetchData();
-    }, []);
+  const handleSearch = () => {
+    const result = dataPakaian.filter((item: Pakaian) => {
+      const nameMatch = item.name.toLowerCase().includes(query.toLowerCase());
+      return nameMatch;
+    });
 
-    const handleSearch = () => {
-        const result = dataPakaian.filter((item: Pakaian) => {
-            const nameMatch = item.name
-                .toLowerCase()
-                .includes(query.toLowerCase());
-            return nameMatch;
-        });
+    setFilteredData(result);
+  };
 
-        setFilteredData(result);
-    };
+  const updateFilteredData = (startPrices: number[], endPrices: number[]) => {
+    const filter = startPrices.map((startPrice, index) => {
+      const temp = dataPakaian.filter((item: Pakaian) => {
+        const endPrice = endPrices[index];
+        return item.price >= startPrice && item.price <= endPrice;
+      });
 
-    const updateFilteredData = (startPrices: number[], endPrices: number[]) => {
-        const filter = startPrices.map((startPrice, index) => {
-            const temp = dataPakaian.filter((item: Pakaian) => {
-                const endPrice = endPrices[index];
-                return item.price >= startPrice && item.price <= endPrice;
-            });
+      return temp;
+    });
 
-            return temp;
-        });
+    const flattened = filter.flat();
+    const result = Array.from(new Set(flattened));
+    setFilteredData(result);
+  };
 
-        const flattened = filter.flat();
-        const result = Array.from(new Set(flattened));
-        setFilteredData(result);
-    };
+  const updateDataToOriginal = () => {
+    setFilteredData(dataPakaian);
+  };
 
-    const updateDataToOriginal = () => {
-        setFilteredData(dataPakaian);
-    };
-
-    return (
-        <>
-            <div className="w-full min-h-screen mb-[50px] bg-backgroundcolor">
-                <div className="container mx-auto max-w-screen-lg">
-                    <h1 className="font-bold text-3xl mt-[100px] font-raleway text-h2">
-                        Pakaian
-                    </h1>
-                    <div className="flex flex-row justify-between gap-6 items-end">
-                        <div className="flex items-center border border-black hover:bg-white rounded-md w-full px-4 mt-4 h-14">
-                            <FaSearch size={18} />
-                            <input
-                                type="text"
-                                placeholder="Cari Pakaian"
-                                className="px-4 py-2 outline-none bg-transparent w-full active:outline-none"
-                                value={query}
-                                onChange={(e) => setQuery(e.target.value)}
-                                onKeyDown={(e) => {
-                                    if (e.key === "Enter") {
-                                        handleSearch();
-                                    }
-                                }}
-                            />
-                        </div>
-                        <Dropdown
-                            updateFilteredData={updateFilteredData}
-                            updateDataToOriginal={updateDataToOriginal}
-                        />
-                    </div>
-                    <div className="grid grid-cols-4 gap-4">
-                        {currentItems.map((pakaian: Pakaian) => (
-                            <PakaianComponent
-                                pakaian={pakaian}
-                                key={pakaian.id}
-                            />
-                        ))}
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="w-full min-h-screen mb-[50px] bg-backgroundcolor">
+        <div className="container mx-auto max-w-screen-lg">
+          <h1 className="font-bold text-3xl mt-[100px] font-raleway text-h2">
+            Pakaian
+          </h1>
+          <div className="flex flex-row justify-between gap-6 items-end">
+            <div className="flex items-center border border-black hover:bg-white rounded-md w-full px-4 mt-4 h-14">
+              <FaSearch size={18} />
+              <input
+                type="text"
+                placeholder="Cari Pakaian"
+                className="px-4 py-2 outline-none bg-transparent w-full active:outline-none"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleSearch();
+                  }
+                }}
+              />
             </div>
-
-            <Pagination
-                filteredData={filteredData}
-                currentPage={currentPage}
-                itemsPerPage={itemsPerPage}
-                paginate={onHandlePage}
+            <Dropdown
+              updateFilteredData={updateFilteredData}
+              updateDataToOriginal={updateDataToOriginal}
             />
-        </>
-    );
+          </div>
+          <div className="grid grid-cols-4 gap-4">
+            {currentItems.map((pakaian: Pakaian) => (
+              <PakaianComponent pakaian={pakaian} key={pakaian.id} />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <Pagination
+        filteredData={filteredData}
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+        paginate={onHandlePage}
+      />
+    </>
+  );
 };
 
 export default KatalogPakaian;
--- src/app/(pages)/(loggedIn)/transaksi/components/Transaksi.tsx
+++ src/app/(pages)/(loggedIn)/transaksi/components/Transaksi.tsx
@@ -4,253 +4,154 @@ import { FaSearch } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import CardTransaksi from "./CardTransaksi";
 import Pagination from "@/components/Pagination";
+import FilterTransaksi from "../../riwayat-transaksi/component/FilterTransaksi";
 
 interface Transaksi {
-    id: string;
-    nama: string;
-    tanggal: string;
-    status: string;
-    total_harga: Number;
-    userId: string;
-    nama_customer: string;
+  id: string;
+  nama: string;
+  tanggal: string;
+  status: string;
+  total_harga: Number;
+  userId: string;
+  nama_customer: string;
 }
 
 async function getDataTransaksi() {
-    const res = await fetch("/api/transaksi", {
-        method: "GET",
-    });
-    const dataTransaksi = await res.json();
-    return dataTransaksi;
+  const res = await fetch("/api/transaksi", {
+    method: "GET",
+  });
+  const dataTransaksi = await res.json();
+  return dataTransaksi;
 }
 
 export default function Transaksi() {
-    const [dataTransaksi, setDataTransaksi] = useState<Transaksi[] | null>(
-        null
-    );
-    const [query, setQuery] = useState("");
-    const [filteredData, setFilteredData] = useState(dataTransaksi); // State untuk menyimpan data yang telah difilter
-    const [currentPage, setCurrentPage] = useState(1); // State untuk menyimpan data halaman yang sedang dibuka
-    const itemsPerPage = 5; // Jumlah item per halaman
+  const [dataTransaksi, setDataTransaksi] = useState<Transaksi[] | null>(null);
+  const [query, setQuery] = useState("");
+  const [filteredData, setFilteredData] = useState(dataTransaksi); // State untuk menyimpan data yang telah difilter
+  const [currentPage, setCurrentPage] = useState(1); // State untuk menyimpan data halaman yang sedang dibuka
+  const itemsPerPage = 5; // Jumlah item per halaman
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await getDataTransaksi();
-                setDataTransaksi(data);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-                // Handle error if needed
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    const [statusFilters, setStatusFilters] = useState({
-        done: false,
-        onProgress: false,
-        notConfirmed: false,
-    });
-
-    const [startDate, setStartDate] = useState("");
-    const [endDate, setEndDate] = useState("");
-    const handleSearch = () => {
-        if (!dataTransaksi) return;
-        const result = dataTransaksi.filter((item: Transaksi) => {
-            const nameMatch = item.nama
-                .toLowerCase()
-                .includes(query.toLowerCase());
-
-            const statusMatch =
-                (!statusFilters.done &&
-                    !statusFilters.onProgress &&
-                    !statusFilters.notConfirmed) ||
-                (statusFilters.done && item.status === "Done") ||
-                (statusFilters.onProgress && item.status === "On Progress") ||
-                (statusFilters.notConfirmed && item.status === "Not Confirmed");
-
-            const dateMatch =
-                (!startDate || new Date(item.tanggal) >= new Date(startDate)) &&
-                (!endDate || new Date(item.tanggal) <= new Date(endDate));
-
-            return nameMatch && statusMatch && dateMatch;
-        });
-
-        setFilteredData(result);
-        setCurrentPage(1);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getDataTransaksi();
+        setDataTransaksi(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
 
-    // Logika untuk mengatur data yang ditampilkan pada setiap halaman
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems =
-        filteredData !== null
-            ? filteredData.slice(indexOfFirstItem, indexOfLastItem)
-            : [];
+    fetchData();
+  }, []);
 
-    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+  const [statusFilters, setStatusFilters] = useState({
+    done: false,
+    onProgress: false,
+    notConfirmed: false,
+  });
 
-    useEffect(() => {
-        setFilteredData(dataTransaksi);
-    }, [dataTransaksi]);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const handleSearch = () => {
+    if (!dataTransaksi) return;
+    const result = dataTransaksi.filter((item: Transaksi) => {
+      const nameMatch = item.nama.toLowerCase().includes(query.toLowerCase());
 
-    return (
-        <>
-            <div className="min-h-screen">
-                <div className="w-full mb-[50px]">
-                    <div className="container mx-auto xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md">
-                        <h1 className="font-bold text-h3 mt-[100px]">
-                            Daftar Transaksi
-                        </h1>
-                        <div className="">
-                            <div className="flex flex-row justify-between gap-6 items-end">
-                                <div className="flex border border-black bg-[#EDEDED] justify-between py-1.5 px-3 rounded-md w-full">
-                                    <input
-                                        type="text"
-                                        placeholder="Cari Transaksi"
-                                        className="px-4 py-2 outline-none bg-transparent w-full focus:ring-1"
-                                        value={query}
-                                        onChange={(e) =>
-                                            setQuery(e.target.value)
-                                        }
-                                    />
-                                </div>
-                                <div className="flex justify-end mt-2">
-                                    <button
-                                        className="px-4 py-2 bg-transparant rounded-md border text-black border-black font-semibold h-14 active:bg-primary active:text-white"
-                                        onClick={handleSearch}
-                                    >
-                                        <FaSearch size={18} />
-                                    </button>
-                                </div>
-                            </div>
+      const statusMatch =
+        (!statusFilters.done &&
+          !statusFilters.onProgress &&
+          !statusFilters.notConfirmed) ||
+        (statusFilters.done && item.status === "Done") ||
+        (statusFilters.onProgress && item.status === "On Progress") ||
+        (statusFilters.notConfirmed && item.status === "Not Confirmed");
 
-                            <div className="flex flex-col mt-2">
-                                <h1 className="font-semibold text-h6 mb-4">
-                                    Filter :
-                                </h1>
-                                <div className="flex flex-row gap-16 item justify-center md:justify-start">
-                                    <div className="flex flex-col">
-                                        <h2 className="font-semibold mb-2">
-                                            Status Transaksi
-                                        </h2>
-                                        <label>
-                                            <input
-                                                type="checkbox"
-                                                className="mr-2"
-                                                checked={statusFilters.done}
-                                                onChange={() =>
-                                                    setStatusFilters(
-                                                        (prevFilters) => ({
-                                                            ...prevFilters,
-                                                            done: !prevFilters.done,
-                                                        })
-                                                    )
-                                                }
-                                            />
-                                            Done
-                                        </label>
-                                        <label>
-                                            <input
-                                                type="checkbox"
-                                                className="mr-2"
-                                                checked={
-                                                    statusFilters.onProgress
-                                                }
-                                                onChange={() =>
-                                                    setStatusFilters(
-                                                        (prevFilters) => ({
-                                                            ...prevFilters,
-                                                            onProgress:
-                                                                !prevFilters.onProgress,
-                                                        })
-                                                    )
-                                                }
-                                            />
-                                            On Progress
-                                        </label>
-                                        <label>
-                                            <input
-                                                type="checkbox"
-                                                className="mr-2"
-                                                checked={
-                                                    statusFilters.notConfirmed
-                                                }
-                                                onChange={() =>
-                                                    setStatusFilters(
-                                                        (prevFilters) => ({
-                                                            ...prevFilters,
-                                                            notConfirmed:
-                                                                !prevFilters.notConfirmed,
-                                                        })
-                                                    )
-                                                }
-                                            />
-                                            Not Confirmed
-                                        </label>
-                                    </div>
-                                    <div className="">
-                                        <h2 className="font-semibold mb-2">
-                                            Tanggal Transaksi
-                                        </h2>
-                                        <div className="flex flex-row gap-2 items-center">
-                                            <input
-                                                type="date"
-                                                className="border border-black rounded-md px-2 py-1"
-                                                value={startDate}
-                                                onChange={(e) =>
-                                                    setStartDate(e.target.value)
-                                                }
-                                            />
-                                            <p>to</p>
-                                            <input
-                                                type="date"
-                                                className="border border-black rounded-md px-2 py-1"
-                                                value={endDate}
-                                                onChange={(e) =>
-                                                    setEndDate(e.target.value)
-                                                }
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+      const dateMatch =
+        (!startDate || new Date(item.tanggal) >= new Date(startDate)) &&
+        (!endDate || new Date(item.tanggal) <= new Date(endDate));
+
+      return nameMatch && statusMatch && dateMatch;
+    });
+
+    setFilteredData(result);
+    setCurrentPage(1);
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems =
+    filteredData !== null
+      ? filteredData.slice(indexOfFirstItem, indexOfLastItem)
+      : [];
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  useEffect(() => {
+    setFilteredData(dataTransaksi);
+  }, [dataTransaksi]);
+
+  return (
+    <>
+      <div className="min-h-screen">
+        <div className="w-full mb-[50px]">
+          <div className="container mx-auto xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md">
+            <h1 className="font-bold text-h3 mt-[100px]">Daftar Transaksi</h1>
+            <div className="">
+              <div className="flex flex-row justify-between gap-6 items-end">
+                <div className="flex border border-black bg-[#EDEDED] justify-between py-1.5 px-3 rounded-md w-full">
+                  <input
+                    type="text"
+                    placeholder="Cari Transaksi"
+                    className="px-4 py-2 outline-none bg-transparent w-full focus:ring-1"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                  />
                 </div>
-                <div className="container mx-auto flex flex-col gap-4 xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md">
-                    {dataTransaksi !== null ? (
-                        currentItems.length > 0 ? (
-                            currentItems.map((item: Transaksi) => (
-                                <CardTransaksi
-                                    key={item.id}
-                                    id={item.id}
-                                    nama={item.nama}
-                                    tanggal={item.tanggal}
-                                    status={item.status}
-                                    total_harga={item.total_harga}
-                                    nama_customer={item.nama_customer}
-                                />
-                            ))
-                        ) : (
-                            <h2 className="font-semibold text-center">
-                                Tidak ada transaksi
-                            </h2>
-                        )
-                    ) : (
-                        <h2 className="font-semibold text-center animate-pulse">
-                            Loading...
-                        </h2>
-                    )}
+                <div className="flex justify-end mt-2">
+                  <FilterTransaksi
+                    statusFilters={statusFilters}
+                    startDate={startDate}
+                    endDate={endDate}
+                    setStatusFilters={setStatusFilters}
+                    setStartDate={setStartDate}
+                    setEndDate={setEndDate}
+                    handleSearch={handleSearch}
+                  />
                 </div>
-                {/* Pagination Controls */}
-                <Pagination
-                    filteredData={filteredData || []}
-                    itemsPerPage={itemsPerPage}
-                    currentPage={currentPage}
-                    paginate={paginate}
-                />
+              </div>
             </div>
-        </>
-    );
+          </div>
+        </div>
+        <div className="container mx-auto flex flex-col gap-4 xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md">
+          {dataTransaksi !== null ? (
+            currentItems.length > 0 ? (
+              currentItems.map((item: Transaksi) => (
+                <CardTransaksi
+                  key={item.id}
+                  id={item.id}
+                  nama={item.nama}
+                  tanggal={item.tanggal}
+                  status={item.status}
+                  total_harga={item.total_harga}
+                  nama_customer={item.nama_customer}
+                />
+              ))
+            ) : (
+              <h2 className="font-semibold text-center">Tidak ada transaksi</h2>
+            )
+          ) : (
+            <h2 className="font-semibold text-center animate-pulse">
+              Loading...
+            </h2>
+          )}
+        </div>
+        {/* Pagination Controls */}
+        <Pagination
+          filteredData={filteredData || []}
+          itemsPerPage={itemsPerPage}
+          currentPage={currentPage}
+          paginate={paginate}
+        />
+      </div>
+    </>
+  );
 }
