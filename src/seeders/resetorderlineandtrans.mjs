ceab7d1b553cf68af4938ea21df0803aad7a5b56
Rasyadan Faza <18221103@std.stei.itb.ac.id>
2023-11-23 09:46:29 +0700

    fix: fix seeders bug

--- src/seeders/resetorderlineandtrans.mjs
+++ src/seeders/resetorderlineandtrans.mjs
@@ -5,7 +5,16 @@ const prisma = new PrismaClient();
 async function main() {
     await prisma.orderline.deleteMany();
     await prisma.transaksi.deleteMany();
-    await prisma.keranjang.deleteMany();
+    const user = await prisma.user.findUnique({
+        where: {
+            email: "test12345@gmail.com",
+        },
+    });
+    await prisma.keranjang.delete({
+        where: {
+            userId: user.id,
+        },
+    });
     await prisma.user.delete({
         where: {
             email: "test12345@gmail.com",
