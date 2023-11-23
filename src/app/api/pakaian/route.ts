2c5e239a7eb2a4ff024e6a14ed290a52a2b9755d
frendysanusi <partner.frendysanusi@gmail.com>
2023-11-23 21:04:02 +0700

    feat: add handle image and add api get by id

--- src/app/api/pakaian/route.ts
+++ src/app/api/pakaian/route.ts
@@ -3,10 +3,57 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET() {
+export async function GET( req: NextRequest ) {
     try {
-        const data = await prisma.existingPakaian.findMany();
+        const params = new URLSearchParams(req.nextUrl.search);
+        const id = params.get("id");
+
+        let data;
+
+        if (id) {
+            data = await prisma.existingPakaian.findUnique({
+                where: {
+                    id: id
+                }
+            });
+        }
+        else {
+            data = await prisma.existingPakaian.findMany();
+        }
+
         return NextResponse.json(data, { status: 200 })
+    }
+    catch (err) {
+        console.error(err);
+        return NextResponse.json({ error: "Error occured." }, { status: 403 })
+    }
+    finally {
+        await prisma.$disconnect();
+    }
+}
+
+export async function POST( req: NextRequest ) {
+    const body = await req.json();
+    const name = body.name;
+    const price = body.price;
+    const unit = body.unit;
+    
+    try {
+        const data = await prisma.pakaian.create({
+            data: {
+                name: name,
+                price: price,
+                unit: unit,
+                existingPakaian: {
+                    create: {
+                        name: name,
+                        price: price,
+                        unit: unit,
+                    }
+                }
+            }
+        });
+        return NextResponse.json(data, { status: 200 });
     }
     catch (err) {
         console.error(err);
