fd827bec84e93749db47c6acebcd702990f21079
jon <zhenagajo234@gmail.com>
2023-11-25 00:10:28 +0700

    fix: edit schema & move cart to loggedin

--- src/app/(pages)/(loggedIn)/cart/page.tsx
+++ src/app/(pages)/(loggedIn)/cart/page.tsx
@@ -1,7 +1,12 @@
-export default function CartPage() {
-  return (
-    <>
-      <h1 className="text-4xl font-bold min-h-screen flex justify-center items-center">Cart Page</h1>
-    </>
-  )
+import CartPage from "./components/CartPage";
+
+export default function Home(){
+    return (
+            <div>
+                <CartPage/>
+            </div>
+    )
 }
+
+
+
