d36d8a836c2b2c611bf927f40baf2439d2bfc397
jon <zhenagajo234@gmail.com>
2023-11-24 11:20:04 +0700

    refactor: add updateCart

--- src/app/api/keranjang/route.ts
+++ src/app/api/keranjang/route.ts
@@ -35,3 +35,35 @@ export async function POST(req:NextRequest){
 
 }
 
+export async function PUT(req:NextRequest){
+    const {ClothesCartData,user} = await req.json();
+
+    // find keranjang first
+
+    const keranjang = await prisma.keranjang.findUnique({
+        where:{
+            userId:user.id
+        }
+    })
+
+    for(let i = 0;i < ClothesCartData.length;i++){
+        // update each pakaian
+        const updatePakaian = await prisma.orderline.updateMany({
+            where:{
+                pakaianId:ClothesCartData[i].pakaianId,
+                keranjangId:keranjang?.id
+            },data:{
+                kuantitas:ClothesCartData[i].kuantitas,
+                total_harga:ClothesCartData[i].total_harga
+            }
+        })
+    }
+
+    return NextResponse.json(
+        {message:"Success"},
+        {status:200}
+    )
+}
+
+
+
