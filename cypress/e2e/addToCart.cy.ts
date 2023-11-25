866594bf18fb3f5a43565881a3d51041346035d2
jon <zhenagajo234@gmail.com>
2023-11-26 00:00:52 +0700

    test: Only add to cart. Haven't checked it out on the cart itself yet

---
+++ cypress/e2e/addToCart.cy.ts
@@ -0,0 +1,28 @@
+
+describe('Add some clothes to cart',()=>{
+
+    beforeEach(() => {
+        cy.loginWithTestAccount("addToCart@gmail.com", "addToCart");
+
+    });
+
+
+    // add some clothes
+    it('It should add some clothes',()=>{
+        cy.get('*[class^="border flex flex-col justify-between my-5 items-start gap-4 shadow-md transition-transform "]')
+            .find('button')
+            .each(($button)=>{
+                cy.wrap($button).click();
+            });
+
+        cy.get('*[class^="text-button px-4 py-2 rounded-md font-raleway bg-secondary-400 hover:bg-secondary-300 active:bg-secondary-300 shadow-lg w-[300px] h-[40px]"]')
+            .click();
+        
+
+        
+        
+
+        
+    })
+
+})
