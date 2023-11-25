4fe06e565dd25ecbdc5b0aa7a8f9a5bba713eb15
frendysanusi <partner.frendysanusi@gmail.com>
2023-11-25 16:44:38 +0700

    test: add more testing

--- cypress/e2e/katalogPakaianAdmin.cy.ts
+++ cypress/e2e/katalogPakaianAdmin.cy.ts
@@ -1,50 +1,61 @@
-describe("Katalog Pakaian", () => {
+describe("Katalog Pakaian Admin", () => {
     beforeEach(() => {
-        cy.loginWithTestAccount("fren@gmail.com", "fren");
+        cy.loginWithTestAccount("admin@gmail.com", "admin123");
         cy.visit("/catalog");
         cy.wait(500);
-    });
+    })
 
     it("should display katalog", () => {
         cy.get("#katalog_pakaian")
             .should("exist")
             .should("be.visible")
             .contains("Pakaian");
-    });
+    })
 
     it("should display search bar", () => {
         cy.get("#search_bar").should("exist").should("be.visible");
-    });
+    })
 
     it("should display filter dropdown", () => {
         cy.get("#filter_dropdown").should("exist").should("be.visible");
-    });
+    })
 
     it("should display pakaian card", () => {
         cy.get("#pakaian_card").should("exist").should("be.visible");
-    });
+    })
 
     it("should search pakaian", () => {
         cy.get("#search_bar").type("kemeja");
         cy.get("#pakaian_card").should("be.visible").contains("Kemeja");
         cy.get("#pakaian_card").should("be.visible").contains("Celana jeans");
-    });
+    })
 
     it("should filter pakaian", () => {
         cy.get("#filter_dropdown").click();
         cy.get("#harga1").click();
         cy.get("#apply").click();
         cy.get("#pakaian_card").should("be.visible").contains("Kemeja");
-    });
+    })
 
-    it("should display add to cart button", () => {
+    it("should display edit button", () => {
         cy.get("#pakaian_card")
             .should("be.visible")
             .contains("Kemeja")
             .parent()
             .parent()
-            .find("#tambah_button")
-            .click()
-        cy.get("#add_to_cart").should("exist").should("be.visible");
+        cy.get("#edit_button").should("exist").should("be.visible");
+    })
+
+    it("should display delete button", () => {
+        cy.get("#pakaian_card")
+            .should("be.visible")
+            .contains("Kemeja")
+            .parent()
+            .parent()
+        cy.get("#delete_button").should("exist").should("be.visible");
+    })
+
+    it("should display create button", () => {
+        cy.get("#create_button").should("exist").should("be.visible");
     })
 })
