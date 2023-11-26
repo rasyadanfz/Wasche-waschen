01e10e0f1fc6d697de8ee809c74b30d6a1e596bd
Rasyadan Faza <18221103@std.stei.itb.ac.id>
2023-11-26 07:58:53 +0700

    fix: fix tests and seeders

--- cypress/e2e/katalogPakaianAdmin.cy.ts
+++ cypress/e2e/katalogPakaianAdmin.cy.ts
@@ -3,61 +3,61 @@ describe("Katalog Pakaian Admin", () => {
         cy.loginWithTestAccount("admin@gmail.com", "admin123");
         cy.visit("/catalog");
         cy.wait(500);
-    })
+    });
 
     it("should display katalog", () => {
         cy.get("#katalog_pakaian")
             .should("exist")
             .should("be.visible")
             .contains("Pakaian");
-    })
+    });
 
     it("should display search bar", () => {
         cy.get("#search_bar").should("exist").should("be.visible");
-    })
+    });
 
     it("should display filter dropdown", () => {
         cy.get("#filter_dropdown").should("exist").should("be.visible");
-    })
+    });
 
     it("should display pakaian card", () => {
         cy.get("#pakaian_card").should("exist").should("be.visible");
-    })
+    });
 
     it("should search pakaian", () => {
         cy.get("#search_bar").type("kemeja");
         cy.get("#pakaian_card").should("be.visible").contains("Kemeja");
         cy.get("#pakaian_card").should("be.visible").contains("Celana jeans");
-    })
+    });
 
     it("should filter pakaian", () => {
         cy.get("#filter_dropdown").click();
         cy.get("#harga1").click();
         cy.get("#apply").click();
         cy.get("#pakaian_card").should("be.visible").contains("Kemeja");
-    })
+    });
 
     it("should display edit button", () => {
         cy.get("#pakaian_card")
             .should("be.visible")
             .contains("Kemeja")
             .parent()
-            .parent()
+            .parent();
         cy.get("#edit_button").should("exist").should("be.visible");
-    })
+    });
 
     it("should display delete button", () => {
         cy.get("#pakaian_card")
             .should("be.visible")
             .contains("Kemeja")
             .parent()
-            .parent()
+            .parent();
         cy.get("#delete_button").should("exist").should("be.visible");
-    })
+    });
 
     it("should display create button", () => {
         cy.get("#create_button").should("exist").should("be.visible");
-    })
+    });
 
     it("should create pakaian", () => {
         cy.get("#create_button").click();
@@ -68,15 +68,15 @@ describe("Katalog Pakaian Admin", () => {
         cy.wait(500);
         cy.get("#katalog_pakaian")
             .should("be.visible")
-            .contains("Test pakaian")
-    })
+            .contains("Test pakaian");
+    });
 
     it("should edit pakaian", () => {
         cy.get("#pakaian_card")
             .should("be.visible")
             .contains("Kemeja")
             .parent()
-            .parent()
+            .parent();
         cy.get("#edit_button").click();
         cy.get("input[id=price]").type("1000");
         cy.get("button[id=submit]").click();
@@ -85,19 +85,18 @@ describe("Katalog Pakaian Admin", () => {
             .should("be.visible")
             .contains("Kemeja")
             .parent()
-            .contains("1000")
-    })
+            .contains("1000");
+    });
 
     it("should delete pakaian", () => {
         cy.get("#pakaian_card")
             .should("be.visible")
             .contains("Test pakaian")
             .parent()
-            .parent()
-        cy.get("#delete_button").click();
+            .parent();
+        cy.get("#pakaian_card>div").last().find("#delete_button").click();
         cy.get("#delete").click();
         cy.wait(1000);
-        cy.get("#katalog_pakaian")
-            .should("not.contain", "Test pakaian")
-    })
-})
+        cy.get("#katalog_pakaian").should("not.contain", "Test pakaian");
+    });
+});
