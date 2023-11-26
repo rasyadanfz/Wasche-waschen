c74bba8cd7412c6851a4c49612ed991c3b873d93
jon <zhenagajo234@gmail.com>
2023-11-26 08:24:49 +0700

    test: finish making testcase for addToCart. There's some error in the logic of API orderline, i guess

--- cypress/e2e/addToCart.cy.ts
+++ cypress/e2e/addToCart.cy.ts
@@ -2,7 +2,7 @@ import { Pakaian } from "@prisma/client";
 
 describe('Add some clothes to cart',()=>{
 
-    let pakaianFromDatabase;
+    let pakaianFromDatabase:any;
 
     beforeEach(() => {
         cy.loginWithTestAccount("addToCart@gmail.com", "addToCart");
@@ -34,6 +34,60 @@ describe('Add some clothes to cart',()=>{
     it('The cart should have the same quantity and price',()=>{
         cy.visit('/cart')
         cy.wait(2000)
+
+        cy.get('*[class^="flex flex-row justify-between items-center"]')
+            .each(($div,index)=>{
+                let pakaianName:string;
+                let pakaianQuantity:number;
+                let pakaianTotalHarga:number;
+                cy.wrap($div)
+                    .find('h1.text-h6.mb-2') // Adjust the selector based on your actual HTML structure
+                    .invoke('text')
+                    .then((text) => {
+                    pakaianName = text;
+                });
+
+                
+                cy.wrap($div)
+                    .find('div.flex.flex-row.gap-5')
+                    .find('div')
+                    .eq(2)
+                    .find('p')
+                    .eq(0)
+                    .invoke('text')
+                    .then((text)=>{
+                        const number = parseInt(text);
+                        expect(number).to.be.a('number');
+                        pakaianQuantity = number;
+                    })
+                
+                cy.wrap($div)
+                    .find('div.flex.flex-row.gap-5')
+                    .find('div')
+                    .eq(2)
+                    .find('p')
+                    .eq(1)
+                    .invoke('text')
+                    .then((text)=>{
+                        const number = parseInt(text);
+                        expect(number).to.be.a('number');
+                        pakaianTotalHarga = number;
+                    })
+                    
+                    
+                cy.then(()=>{
+                    cy.log(`Pakaian ${pakaianName} has the quantity of ${pakaianQuantity} with total harga of ${pakaianTotalHarga}`)
+
+                    for(let i = 0;i < pakaianFromDatabase.length;i++){
+                        if(pakaianName === pakaianFromDatabase[i].name){
+                            // same
+                            expect(pakaianTotalHarga).to.equal(pakaianFromDatabase[i].price*pakaianQuantity)
+                        }
+                    }
+
+                })
+
+            })
 
         
         
