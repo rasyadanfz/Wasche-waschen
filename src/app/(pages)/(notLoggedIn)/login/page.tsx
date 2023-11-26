912abd8adf5e69ede2a54336f63b4164390436bf
Rasyadan Faza <18221103@std.stei.itb.ac.id>
2023-11-26 07:59:12 +0700

    style: change login and register layout

--- src/app/(pages)/(notLoggedIn)/login/page.tsx
+++ src/app/(pages)/(notLoggedIn)/login/page.tsx
@@ -13,7 +13,7 @@ const LoginPage = async () => {
     }
 
     return (
-        <div className="lg:mt-5 lg:mx-20">
+        <div className="lg:mt-5 lg:mx-20 lg:h-[90vh] flex flex-col justify-center">
             <div className="hidden lg:flex justify-between items-center mb-5">
                 <div className="text-h3 font-itim text-primary-400">
                     Wäsche Waschen
@@ -25,7 +25,7 @@ const LoginPage = async () => {
                     Register
                 </Link>
             </div>
-            <div className="lg:grid lg:grid-cols-2 lg:gap-x-12 lg:items-center lg:pb-6 lg:mt-14">
+            <div className="lg:grid lg:grid-cols-2 lg:gap-x-12 lg:items-center lg:my-auto">
                 <div className="flex flex-col px-10 py-4 mt-3 mr-2 font-raleway lg:border-2 lg:border-black rounded-lg lg:bg-[#FBFBFB]">
                     <div className="flex lg:hidden justify-center mb-5">
                         <Image
