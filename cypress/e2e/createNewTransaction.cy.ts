6ee28559172a8a86b8c64dfd8cf3f879a121d29c
jon <zhenagajo234@gmail.com>
2023-11-27 16:45:12 +0700

    test: add some id in cartPage for testing and finished making a test for creating a new Transaction

--- cypress/e2e/createNewTransaction.cy.ts
+++ cypress/e2e/createNewTransaction.cy.ts
@@ -1,9 +1,10 @@
 import type { ClothesCartData } from "@/app/api/keranjang/[id]/route";
+import { equal } from "assert";
 
 describe('Create a new Transaction',()=>{
 
     let cntPakaian = 0;
-    let arrPakaian:ClothesCartData[];
+    let arrPakaian:ClothesCartData[] = [];
 
     beforeEach(()=>{
         cy.loginWithTestAccount("createNewTransaction@gmail.com","createNewTransaction")
@@ -36,7 +37,51 @@ describe('Create a new Transaction',()=>{
         cy.wait(3000)
 
         // traverse through each of clothes
-        cy.get
+        cy.get('#keranjang_card')
+            .children('div')
+            .each(($div)=>{
+
+                let tempElement:ClothesCartData = {
+                    pakaianNama:"ASD",
+                    kuantitas:123,
+                    total_harga:123
+                }
+                
+                cy.wrap($div)
+                    .find('h1')
+                    .invoke('text')
+                    .then((text)=>{
+                        tempElement.pakaianNama = text;
+                        cy.wrap($div)
+                        .find('div > div')
+                        .eq(0)
+                        .find('div > div')
+                        .eq(2)
+                        .children('p')
+                        .each(($p,index)=>{
+                            if(index === 0){
+                                cy.wrap($p)
+                                    .invoke('text')
+                                    .then((text)=>{
+                                        const num = parseInt(text)
+                                        cy.log(`${num}`)
+                                        tempElement.kuantitas = num
+                                    })
+                            }else{
+                                cy.wrap($p)
+                                .invoke('text')
+                                .then((text)=>{
+                                    const num = parseInt(text)
+                                    cy.log(`${num}`)
+                                    tempElement.total_harga = num
+                                })
+                            }
+                        }).then(()=>{
+                            cy.log(`${tempElement.kuantitas} ${tempElement.pakaianNama} ${tempElement.total_harga}`)
+                            arrPakaian.push(tempElement);
+                        })
+                    })
+            })
 
         cy.get('#Create_Order')
             .click()
@@ -55,8 +100,53 @@ describe('Create a new Transaction',()=>{
             .eq(0)
             .find('button')
             .click();
-    
-        cy.wait(5000)
+
+        cy.wait(2000)
+
+        let cntIdx:number = 0;
+        
+        cy.get('table')
+            .find('tbody')
+            .eq(1)
+            .children('tr')
+            .each(($tr)=>{
+                cy.log(`${arrPakaian[cntIdx].kuantitas} ${arrPakaian[cntIdx].pakaianNama} ${arrPakaian[cntIdx].total_harga}`)
+                const temp = arrPakaian[cntIdx];
+                cy.wrap($tr)
+                    .children('td')
+                    .each(($td,index)=>{
+                        if(index === 1){
+                            // compare with name
+                            cy.wrap($td)
+                                .invoke('text')
+                                .then((text)=>{
+                                    expect(text).to.equal(temp.pakaianNama);
+                                })
+                    
+                        }else if(index === 3){
+                            cy.wrap($td)
+                                .invoke('text')
+                                .then((text)=>{
+                                    const num = parseInt(text);
+                                    expect(num).to.equal(temp.kuantitas);
+                                })
+                        }else if(index === 4){
+                            cy.wrap($td)
+                            .invoke('text')
+                            .then((text)=>{
+                                const temptext = text.slice(4);
+                                const num = parseInt(temptext.replace(/,/g,'')); 
+                                expect(num).to.equal(temp.total_harga)
+                            })
+                        }
+                      
+                    })
+                    cntIdx++;
+            }).then(()=>{
+                expect(cntIdx).to.equal(arrPakaian.length)
+            })
+
+
     })
 
 })
