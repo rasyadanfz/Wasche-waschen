d49bcea1ac88912562e5430f5dc779b85c0d6d37
jon <zhenagajo234@gmail.com>
2023-11-27 13:47:00 +0700

    feat : finished totalHarga and disable CreateOrder button

--- src/app/utils/totalharga.ts
+++ src/app/utils/totalharga.ts
@@ -1,7 +1,6 @@
 import type { Orderline } from "@prisma/client";
-import type { ClothesCartData } from "../api/keranjang/[id]/route";
 
-export function calculateTotalHarga(listOrderLine:ClothesCartData[]){
+export function totalHarga(listOrderLine:Orderline[]){
     var sum = 0;
     listOrderLine.forEach((ol)=>{
         sum = (sum + ol.total_harga)
